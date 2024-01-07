70716b2c48c04ba12996b711dfc9ad28ba13b767
allburov <allburov@gmail.com>
2024-01-07 15:00:22 +0700

    Add label if member

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 import {Context, Probot} from "probot";
+import { Octokit } from "@octokit/core";
 
-async function handleIssue(context: Context) {
+async function handleIssue(context: Context<'issues'>) {
     const config = await context.config<GHTeamLabelConfig>('gh-team-labels.yml')
     if (!config) {
         return
     }
-    const labels = config.teamLabels.map((teamLabel) => teamLabel.label)
+    const createdByUser = context.payload.issue.user
+    const orgName = context.payload.repository.owner.login
+    const labels = []
+    for (const teamLabel of config.teamLabels) {
+        const isMember = await isMemberOf(context.octokit, orgName, createdByUser.login, teamLabel.team)
+        if (isMember) {
+            labels.push(teamLabel.label)
+        }
+    }
     await context.octokit.issues.addLabels(context.issue({labels: labels}))
+}
+
+async function isMemberOf(octokit: Octokit, orgName: string, userName: string, teamSlug: string): Promise<boolean> {
+    const teamResponse = await octokit.request('GET /orgs/{org}/teams/{team_slug}/memberships/{username}', {
+        org: orgName,
+        team_slug: teamSlug,
+        username: userName
+    })
+    return teamResponse.status === 200
 }
 
 

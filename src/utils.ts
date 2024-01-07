dd27629140ce40a08433e0d03397756bcbfec17b
allburov <allburov@gmail.com>
2024-01-07 15:20:20 +0700

    Fix http 404

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,24 @@
 import {Octokit} from "@octokit/core";
 import {GHTeamLabelConfig} from "./config";
+import {RequestError} from "@octokit/request-error";
 
 export async function isMemberOf(octokit: Octokit, orgName: string, userName: string, teamSlug: string): Promise<boolean> {
-    const teamResponse = await octokit.request('GET /orgs/{org}/teams/{team_slug}/memberships/{username}', {
-        org: orgName,
-        team_slug: teamSlug,
-        username: userName
-    })
-    return teamResponse.status === 200
+    try {
+        const teamResponse = await octokit.request('GET /orgs/{org}/teams/{team_slug}/memberships/{username}', {
+            org: orgName,
+            team_slug: teamSlug,
+            username: userName
+        })
+        return teamResponse.status === 200
+    }
+    catch (err) {
+        if ((err instanceof RequestError) && (err.status === 404)) {
+            return false
+        }
+        throw err
+    }
 }
+
 export async function getFirstSuitableLabel(
     octokit: Octokit,
     config: GHTeamLabelConfig,

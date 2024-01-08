1ce485c0b77f959c817cdf8a2e06be48bd2b46a4
allburov <allburov@gmail.com>
2024-01-08 18:40:09 +0700

    Add discussion badges

--- src/handlers/badges.ts
+++ src/handlers/badges.ts
@@ -42,6 +42,33 @@ export async function handlePullRequestAddBadge(context: Context<'pull_request'>
     await context.octokit.issues.update(context.issue({body: newBody}))
 }
 
+export async function handleDiscussionAddBadge(context: Context<'discussion'>) {
+    const config = await getConfig(context);
+    const createdByUser = context.payload.discussion.user
+    const orgName = context.payload.repository.owner.login
+    const teamLabel = await getFirstSuitableLabel(context.octokit, config, orgName, createdByUser.login, [])
+    const body = context.payload.discussion.body || ""
+    const newBody = upsertBadge(body, teamLabel)
+    if (body === newBody) {
+        return;
+    }
+    const discussionId = context.payload.discussion.node_id
+    const mutation = `
+  mutation updateDiscussionBody($discussionId: ID!, $newBody: String!) {
+    updateDiscussion(input: {discussionId: $discussionId, body: $newBody}){
+        discussion {
+            id
+        }
+    }
+  }
+`;
+
+    await context.octokit.graphql(mutation, {
+        discussionId: discussionId,
+        newBody: newBody,
+    });
+}
+
 function upsertBadge(body: string, teamLabel: GHTeamLabel | null) {
     if (!teamLabel) {
         return body;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import {Probot} from "probot";
-import {handleIssueAddBadge, handleIssueCommentAddBadge, handlePullRequestAddBadge} from "./handlers/badges";
+import {
+    handleDiscussionAddBadge,
+    handleIssueAddBadge,
+    handleIssueCommentAddBadge,
+    handlePullRequestAddBadge
+} from "./handlers/badges";
 import {handleIssueLabels, handleIssueLabelsOnCommentEvent, handlePullRequestLabels} from "./handlers/labels";
 
 
@@ -12,6 +17,8 @@ export = (app: Probot) => {
     app.on("issue_comment.edited", handleIssueCommentAddBadge);
     app.on("pull_request.opened", handlePullRequestAddBadge);
     app.on("pull_request.edited", handlePullRequestAddBadge);
+    app.on("discussion.created", handleDiscussionAddBadge);
+    app.on("discussion.edited", handleDiscussionAddBadge);
     // Labels
     app.on("issues.opened", handleIssueLabels);
     app.on("issues.reopened", handleIssueLabels);

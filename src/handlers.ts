893e7720cfc6afb21464170c9e01d4b151d9fa24
allburov <allburov@gmail.com>
2024-01-08 11:32:13 +0700

    Add badge to comment

--- src/handlers.ts
+++ src/handlers.ts
@@ -9,13 +9,40 @@ export async function handleIssueLabels(context: Context<'issues'>) {
     const orgName = context.payload.repository.owner.login
     const currentLabels = context.payload.issue.labels?.map(label => label.name) || []
     const teamLabel = await getFirstSuitableLabel(context.octokit, config, orgName, createdByUser.login, currentLabels)
-    if (teamLabel) {
-        const label = teamLabel.label
-        await context.octokit.issues.addLabels(context.issue({labels: [label]}))
+    if (!teamLabel) {
+        return;
     }
+
+    const label = teamLabel.label
+    await context.octokit.issues.addLabels(context.issue({labels: [label]}))
 }
 
 export async function handleIssueLabelsOnCommentEvent(context: Context<'issue_comment'>) {
     // @ts-ignore
     await handleIssueLabels(context)
+}
+
+/**
+ * Add a badge to the issue comment at the end of the comment text
+ * @param context
+ */
+export async function handleIssueCommentAddBadge(context: Context<'issue_comment.created'> | Context<'issue_comment.edited'>) {
+    const config = await getConfig(context);
+    const createdByUser = context.payload.comment.user
+    const orgName = context.payload.repository.owner.login
+    const teamLabel = await getFirstSuitableLabel(context.octokit, config, orgName, createdByUser.login, [])
+    if (!teamLabel) {
+        return;
+    }
+    const badge = teamLabel.comment
+    if (!badge) {
+        return;
+    }
+    const commentBody = context.payload.comment.body
+    if (commentBody.includes(badge)) {
+        return;
+    }
+    const newCommentBody = commentBody + "\n" + badge
+    const commentId = context.payload.comment.id
+    await context.octokit.issues.updateComment(context.issue({comment_id: commentId, body: newCommentBody}))
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import {Probot} from "probot";
-import {handleIssueLabels, handleIssueLabelsOnCommentEvent} from "./handlers";
+import {handleIssueCommentAddBadge, handleIssueLabels, handleIssueLabelsOnCommentEvent} from "./handlers";
 
 
 
 export = (app: Probot) => {
+    // Comments content
+    app.on("issue_comment.created", handleIssueCommentAddBadge);
+    app.on("issue_comment.edited", handleIssueCommentAddBadge);
+    // Labels
     app.on("issues.opened", handleIssueLabels);
     app.on("issues.reopened", handleIssueLabels);
     app.on("issues.edited", handleIssueLabels);
